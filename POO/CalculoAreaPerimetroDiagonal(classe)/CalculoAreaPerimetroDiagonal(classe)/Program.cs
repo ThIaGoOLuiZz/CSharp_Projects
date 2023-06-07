978244de651c0e71d4450c1ef8c7f51729d3eba6
Thiago Luiz <126069763+ThIaGoOLuiZz@users.noreply.github.com>
2023-06-07 17:14:48 -0300

    Add files via upload

---
+++ POO/CalculoAreaPerimetroDiagonal(classe)/CalculoAreaPerimetroDiagonal(classe)/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CalculoAreaPerimetroDiagonal_classe_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r = new Retangulo();
+
+            Console.Write("Digite o valor da Largura do retangulo: ");
+            r.Largura = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor da Altura do retangulo: ");
+            r.Altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine(r);
+        }
+    }
+}
---
+++ POO/CalculoAreaPerimetroDiagonal(classe)/CalculoAreaPerimetroDiagonal(classe)/Retangulo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CalculoAreaPerimetroDiagonal_classe_
+{
+    internal class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        // Metodo para calcular area
+        public double Area()
+        {
+            return (Largura * Altura);
+        }
+
+        // Metodo para calcular perimetro
+        public double Perimetro()
+        {
+            return (Largura * 2) + (Altura * 2);
+        }
+
+        // Metodo para calcular a diagonal
+        public double Diagonal()
+        {
+            return Math.Sqrt((Largura * Largura) + (Altura * Altura));
+        }
+
+        public override string ToString()
+        {
+            return "AREA = " + Area().ToString("F2", CultureInfo.InvariantCulture) +
+                   "\nPERIMETRO = " + Perimetro().ToString("F2", CultureInfo.InvariantCulture) + 
+                   "\nDIAGONAL = " + Diagonal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
