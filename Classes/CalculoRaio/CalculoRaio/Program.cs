7c065ade4fcd41dc6c52e1fe5bf9aad97d7a0a36
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-01-20 19:49:13 -0300

    Inserido novo projeto de Classes Estaticas

---
+++ Classes/CalculoRaio/CalculoRaio/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalculoRaio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor do Raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine($"Circunferencia: {circ.ToString("F2")}");
+            Console.WriteLine($"Volume: {volume.ToString("F2")}");
+            Console.WriteLine($"PI: {Calculadora.Pi.ToString("F2")}");
+        }
+    }
+}
