0f8d239832337b60b9954b1445ca58b1e267b92c
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-01-20 18:03:30 -0300

    Adicionado novo projeto de classes

---
+++ Classes/AumentoSalario/AumentoSalario/Funcionario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AumentoSalario
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentoSalarial(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto / porcentagem);
+        }
+    }
+}
---
+++ Classes/AumentoSalario/AumentoSalario/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace AumentoSalario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+
+            Console.WriteLine("Digite os dados do funcionario: ");
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine($"Funcionario: {funcionario.Nome}, ${funcionario.SalarioLiquido().ToString("F2")}");
+            Console.WriteLine();
+
+            Console.Write("Digite a porcentagem para aumento do salário: ");
+            funcionario.AumentoSalarial(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine();
+
+            Console.WriteLine($"Dados atualizados: {funcionario.Nome}, ${funcionario.SalarioLiquido().ToString("F2")}");
+
+        }
+    }
+}
