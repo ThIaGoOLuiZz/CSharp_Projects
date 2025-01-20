7be0b2bfb81640d6ea861357df864db89f950079
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-01-20 20:00:20 -0300

    Adicionado novo projeto ConversorDolar

---
+++ Classes/ConversorDolar/ConversorDolar/ConversorDeMoedas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorDolar
+{
+    internal class ConversorDeMoedas
+    {
+        public static double Iof = 6.0;
+
+        public static double CalculaConversao(double cotacao, double valor)
+        { 
+            double valorTotal = (valor * cotacao);
+            double imposto = (Iof / 100) * valorTotal;
+            return valorTotal + imposto;
+        }
+    }
+}
---
+++ Classes/ConversorDolar/ConversorDolar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace ConversorDolar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a cotação do Dólar?: ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar?: ");
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Valor a ser pago: " + ConversorDeMoedas.CalculaConversao(cotacao, valor).ToString("F2"));
+        }
+    }
+}
