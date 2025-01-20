abb12db838cc5b2e448f10ec9ad225ce59558980
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-01-20 18:33:05 -0300

    Adicionado novo projeto de Classes

---
+++ Classes/CalculoNotas/CalculoNotas/Aluno.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoNotas
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public double CalculoNota()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public string ValidaNota(double notaFinal)
+        {
+            if (notaFinal < 60.00)
+            {
+                return $"REPROVADO! Faltaram {(60.00 - notaFinal).ToString("F2")}";
+            }
+            return "APROVADO";
+        }
+    }
+}
---
+++ Classes/CalculoNotas/CalculoNotas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace CalculoNotas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome: ");
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do aluno: ");
+
+            Console.Write("Nota 1: ");
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Nota 2: ");
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Nota 3: ");
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double notaFinal = aluno.CalculoNota();
+
+            Console.WriteLine($"NOTA FINAL: {notaFinal.ToString("F2")}");
+
+            Console.WriteLine(aluno.ValidaNota(notaFinal));
+        }
+    }
+}
