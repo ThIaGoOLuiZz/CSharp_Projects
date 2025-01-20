c1d424e1a13e9d6297192fa0c9bd290976dce86b
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-01-20 16:09:02 -0300

    Adicionado novo projeto de classes: AtualizacoesEstoque

---
+++ POO/AtualizacoesEstoque/AtualizacoesEstoque/Produto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace AtualizacoesEstoque
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotal()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int qtd)
+        {
+            Quantidade += qtd;
+        }
+
+        public void RemoverProdutos(int qtd)
+        {
+            Quantidade -= qtd;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $"
+                + ValorTotal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
