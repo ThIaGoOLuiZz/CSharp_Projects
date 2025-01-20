1c80e9efdda2b38721b63b773a8b59c11fa73e2d
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-01-20 17:48:42 -0300

    Adicionado novo projeto de Classes

---
+++ Classes/CalculosRetangulo/CalculosRetangulo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace CalculosRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Digite os dados do retângulo!");
+
+            Console.Write("Largura: ");
+            retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Altura: ");
+            retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(retangulo.ToString());
+        }
+    }
+}
---
+++ Classes/CalculosRetangulo/CalculosRetangulo/Retangulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculosRetangulo
+{
+    internal class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double CalculoArea()
+        {
+            return Largura * Altura;
+        }
+
+        public double CalculoPerimetro()
+        {
+            return (2 * Altura) + (2 * Largura);
+        }
+
+        public double CalculoDiagonal()
+        {
+            return Math.Sqrt((Largura * Largura) + (Altura * Altura));
+        }
+
+        public override string ToString()
+        {
+            return $"AREA: {CalculoArea().ToString("F2")} \nPERÍMETRO: {CalculoPerimetro().ToString("F2")} \nDIAGONAL: {CalculoDiagonal().ToString("F2")}";
+        }
+    }
+}
