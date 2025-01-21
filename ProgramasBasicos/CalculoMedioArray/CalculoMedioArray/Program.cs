82ac17d0ea95572643a09d7e6026564fe232befe
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-01-21 20:29:47 -0300

    Adicionado novo projeto de Arrays CalculoMedioArray

---
+++ ProgramasBasicos/CalculoMedioArray/CalculoMedioArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace CalculoMedioArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o número de valores: ");
+            int x = int.Parse(Console.ReadLine());
+
+            double[] valores = new double[x];
+
+            for (int i = 0; i < valores.Length; i++)
+            {
+                Console.Write($"Digite o valor da {i + 1}ª pessoa: ");
+                valores[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double total = 0.0;
+            for (int i = 0;i < valores.Length; i++)
+            {
+                total += valores[i];
+            }
+
+            Console.WriteLine($"Média: {(total / x).ToString("F2")}");
+        }
+    }
+}
