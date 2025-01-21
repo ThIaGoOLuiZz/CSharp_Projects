c884e9e09c780fe2342722f206cbdd0fc22458c5
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-01-21 20:45:20 -0300

    Adicionado novo projeto de POO: CalculoMedioProdutos

---
+++ OrientadoObjeto/CalculoValoresProdutos/CalculoValoresProdutos/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoValoresProdutos
+{
+    internal class Produto
+    {
+        public string Nome { get; set; }
+        public double Valor { get; set; }
+
+        public Produto(string nome, double valor)
+        {
+            Nome = nome;
+            Valor = valor;
+        }
+    }
+}
---
+++ OrientadoObjeto/CalculoValoresProdutos/CalculoValoresProdutos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace CalculoValoresProdutos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Quantidade de produtos: ");
+            int qtd = int.Parse(Console.ReadLine());
+
+            Produto[] produtos = new Produto[qtd];
+
+            for (int i = 0; i < qtd; i++)
+            {
+                
+
+                Console.Write("Digite o NOME do produto: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Digite o VALOR do produto: ");
+                double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                produtos[i] = new Produto(nome, valor);
+            }
+
+            double valorTotal = 0.0;
+            for (int i = 0;i < qtd; i++)
+            {
+                valorTotal += produtos[i].Valor;
+            }
+
+            Console.WriteLine($"valor médio: {(valorTotal/qtd).ToString("F2")}");
+        }
+    }
+}
