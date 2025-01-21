b3ef7340c8ad974b55cf0289af20eb3748820ed3
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-01-21 00:29:35 -0300

    Adicionado novo projeto de Conta Bancaria

---
+++ OrientadoObjeto/ContaBancaria/ContaBancaria/ContaBancaria.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancaria
+{
+    internal class ContaBancaria
+    {
+        public int NumeroConta { get; set; }
+        public string Nome { get; set; }
+        public double Valor { get; private set; }
+
+        public ContaBancaria(int numeroConta, string nome)
+        {
+            NumeroConta = numeroConta;
+            Nome = nome;
+            Valor = 0.0;
+        }
+
+        public ContaBancaria(int numeroConta, string nome, double valor) : this(numeroConta, nome)
+        {
+            Valor = valor;
+        }
+
+        public void DepositoBancario (double valor)
+        {
+            Valor += valor;
+        }
+
+        public void SaqueBancario (double valor)
+        {
+            Valor -= valor - 5.0;
+        }
+    }
+}
---
+++ OrientadoObjeto/ContaBancaria/ContaBancaria/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Net.WebSockets;
+using System.Globalization;
+
+namespace ContaBancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o titular da conta: ");
+            string nome = Console.ReadLine();
+
+            bool stValidacao = true;
+            string vlDeposito = "";
+            ContaBancaria conta;
+
+            while (stValidacao)
+            {
+                Console.WriteLine("Haverá depósito inicial?(S/N)");
+                vlDeposito = Console.ReadLine();
+
+                if (vlDeposito.ToUpper() == "S" || vlDeposito.ToUpper() == "N")
+                {
+                    stValidacao = false;
+                }
+                else
+                {
+                    Console.WriteLine("Digite apenas S ou N!");
+                }
+            }
+            if(vlDeposito.ToUpper() == "S")
+            {
+                Console.WriteLine("Digite o valor de depósito");
+                double valorDeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                conta = new ContaBancaria(numeroConta, nome, valorDeposito);
+            }
+            else
+            {
+                conta = new ContaBancaria(numeroConta, nome);
+            }
+
+            Console.WriteLine("Conta Criada com sucesso! Dados da conta: ");
+            Console.WriteLine($"Conta {conta.NumeroConta}, Titular: {conta.Nome}, Saldo: $ {conta.Valor}");
+
+            int vlEscolha = 1;
+
+            while (vlEscolha != 0)
+            {
+                stValidacao = false;
+                while (!stValidacao)
+                {
+                    Console.WriteLine("Escolha a opção desejada: \n1 - Depósito;\n2 - Saque;\n3 - Sair;");
+                    vlEscolha = int.Parse(Console.ReadLine());
+                    if (vlEscolha != 1 && vlEscolha != 2 && vlEscolha != 3)
+                    {
+                        Console.WriteLine("Selecione apenas 1, 2 ou 3!");
+                    }
+                }
+
+                switch (vlEscolha)
+                {
+                    case 1:
+                        Console.Write("Digite o valor para Depósito: ");
+                        double valorDeposito = int.Parse(Console.ReadLine());
+
+                        conta.DepositoBancario(valorDeposito);
+                        break;
+
+                    case 2:
+                        Console.Write("Digite o valor para Saque: ");
+                        double valorSaque = int.Parse(Console.ReadLine());
+
+                        conta.SaqueBancario(valorSaque);
+                        break;
+                    case 3:
+                        vlEscolha = 0;
+                        break;
+                }
+            }
+
+        }
+    }
+}
