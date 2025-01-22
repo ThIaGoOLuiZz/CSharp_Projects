db2107d836e1928ca965fd0a385b229de827ecf3
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-01-21 21:37:09 -0300

    Adicionado novo projeto de POO: AluguelHotel

---
+++ OrientadoObjeto/AluguelHotel/AluguelHotel/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+
+namespace AluguelHotel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"Quantidade de quartos alugados: ");
+            int qtdQuartos = int.Parse( Console.ReadLine());
+
+            Quarto[] quartos = new Quarto[qtdQuartos];
+
+            for ( int i = 0; i < qtdQuartos; i++)
+            {
+                Console.Write("Qual o numero do quarto: ");
+                int numero = int.Parse( Console.ReadLine());
+
+                Console.Write("Nome da pessoa: ");
+                string nome = Console.ReadLine();
+                Console.WriteLine();
+
+                Console.Write("Email da pessoa: ");
+                string email = Console.ReadLine();
+                Console.WriteLine();
+
+                Console.Write("Idade da pessoa: ");
+                int idade = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                Console.Write("Valor do aluguel: ");
+                double valorAluguel = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine();
+
+                Aluguel aluguel = new Aluguel(valorAluguel);
+                Pessoa pessoa = new Pessoa(nome, email, idade);
+                Quarto quarto = new Quarto(numero, pessoa, aluguel);
+
+                quartos[i] = quarto;
+            }
+
+            Console.WriteLine("Quartos ocupados:");
+            for ( int i = 0;i < qtdQuartos;i++)
+            {
+                Console.WriteLine($"Número: {quartos[i].Numero}: Nome: {quartos[i].Pessoa.Nome} | Email: {quartos[i].Pessoa.Email} | Idade: {quartos[i].Pessoa.Idade} | Aluguel: {quartos[i].Aluguel.Valor.ToString("F2")}");
+            }
+        }
+    }
+}
---
+++ OrientadoObjeto/AluguelHotel/AluguelHotel/Quarto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluguelHotel
+{
+    internal class Quarto
+    {
+        public int Numero { get; set; }
+        public Pessoa Pessoa { get; set; }
+        public Aluguel Aluguel { get; set; }
+
+        public Quarto(int numero, Pessoa pessoa, Aluguel aluguel)
+        {
+            Numero = numero;
+            Pessoa = pessoa;
+            Aluguel = aluguel;
+        }
+    }
+}
