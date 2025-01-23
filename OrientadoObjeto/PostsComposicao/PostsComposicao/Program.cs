1bf2980cc6c1576f6650e9d5cc358618b8dd8b74
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-01-23 19:59:37 -0300

    Ädicionado novo projeto de POO e StringBuilder: PostsComposicao

---
+++ OrientadoObjeto/PostsComposicao/PostsComposicao/Program.cs
@@ -0,0 +1,24 @@
+﻿using PostsComposicao.Entities;
+
+namespace PostsComposicao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip!");
+            Comment c2 = new Comment("Wow  that's awesome!");
+
+            Post p1 = new Post(
+                DateTime.Now,
+                "Traveling to New Zealand",
+                "I'm going to visit this wonderful country!",
+                12
+            );
+
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+            Console.WriteLine(p1);
+        }
+    }
+}
