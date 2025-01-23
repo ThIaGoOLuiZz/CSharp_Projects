b5a5389e958533f8432c01eaa77bd2f76018568d
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-01-22 23:05:17 -0300

    Adicionado novo projeto de ENUMS: OrderStatusEnum

---
+++ OrientadoObjeto/OrderStatusEnum/OrderStatusEnum/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿ namespace OrderStatusEnum.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPayments  = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ OrientadoObjeto/OrderStatusEnum/OrderStatusEnum/Program.cs
@@ -0,0 +1,28 @@
+﻿using OrderStatusEnum.Entities;
+using OrderStatusEnum.Entities.Enums;
+
+namespace OrderStatusEnum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayments
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayments.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+
+            Console.WriteLine(txt);
+        }
+    }
+}
