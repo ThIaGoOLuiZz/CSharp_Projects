71a2b89ecaabb749f11b7bea4fbf7b2a1cf5da4a
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-01-23 23:02:41 -0300

    Adicionado polimorfismo no projeto de ContasHeranca

---
+++ OrientadoObjeto/ContasHeranca/ContasHeranca/Entities/SavingsAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ContasHeranca.Entities
+{
+    internal class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
--- OrientadoObjeto/ContasHeranca/ContasHeranca/obj/Debug/net8.0/ContasHeranca.AssemblyInfo.cs
+++ OrientadoObjeto/ContasHeranca/ContasHeranca/obj/Debug/net8.0/ContasHeranca.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContasHeranca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddceba0dfa4ad23cd600b0cf5da699e51de65f8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fbe5e4f3df055ce4fec7191f7f1c0e8f2eb8d04")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContasHeranca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContasHeranca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
