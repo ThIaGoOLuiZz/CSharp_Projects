5fbe5e4f3df055ce4fec7191f7f1c0e8f2eb8d04
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-01-23 22:42:14 -0300

    Adicionado novo projeto de Herança: ContasHeranca

---
+++ OrientadoObjeto/ContasHeranca/ContasHeranca/Program.cs
@@ -0,0 +1,14 @@
+﻿using ContasHeranca.Entities;
+
+namespace ContasHeranca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
