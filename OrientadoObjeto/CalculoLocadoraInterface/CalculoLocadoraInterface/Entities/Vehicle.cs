c673571a4957031b1278547416689bc70f2aeae0
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-01-27 20:27:46 -0300

    Adicionado projeto de POO

---
+++ OrientadoObjeto/CalculoLocadoraInterface/CalculoLocadoraInterface/Entities/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿namespace CalculoLocadoraInterface.Entities
+{
+    internal class Vehicle
+    {
+        public string Model { get; set; }
+
+        public Vehicle(string model)
+        {
+            Model = model;
+        }
+    }
+}
