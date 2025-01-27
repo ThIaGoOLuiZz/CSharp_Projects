fc085fa3cb0bb81d43e6c4b5c44f667e3200dd97
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-01-27 14:55:54 -0300

    Adicionado novo projeto de FileWriter e SteamReader

---
+++ ManipulacaoArquivos/FileStream_StreamReader/FileStream_StreamReader/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+namespace FileStream_StreamReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\thiag\Downloads\File.txt";
+
+            StreamReader sr = null;
+            string line = "";
+            try
+            {
+                sr = File.OpenText(path);
+
+                while (!sr.EndOfStream)
+                {
+                    line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+            }
+        }
+    }
+}
