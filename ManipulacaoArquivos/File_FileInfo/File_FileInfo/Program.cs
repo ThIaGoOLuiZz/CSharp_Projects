0130b9f53948f8a9cb975628484a8ab0351aecee
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-01-27 14:37:19 -0300

    Adicionado novo projeto de Manipulação de arquivos, inserido projeto de File e FileInfo

---
+++ ManipulacaoArquivos/File_FileInfo/File_FileInfo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace File_FileInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\thiag\Downloads\File.txt";
+            string targetPath = @"C:\Users\thiag\Downloads\File2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+
+                fileInfo.CopyTo(targetPath);
+
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error Occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
