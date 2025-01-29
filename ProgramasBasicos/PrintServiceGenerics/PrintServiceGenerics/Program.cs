0b6c12ab123a164b3bda41c8691a328b479c5e05
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-01-28 21:10:49 -0300

    Adicionado projeto de Generics

---
+++ ProgramasBasicos/PrintServiceGenerics/PrintServiceGenerics/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PrintServiceGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService printService = new PrintService();
+
+            Console.Write("How manu values: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+            Console.WriteLine();
+            Console.WriteLine("First: " + printService.First());
+            
+        }
+    }
+}
--- ProgramasBasicos/PrintServiceGenerics/PrintServiceGenerics/obj/Debug/net9.0/PrintServiceGenerics.AssemblyInfo.cs
+++ ProgramasBasicos/PrintServiceGenerics/PrintServiceGenerics/obj/Debug/net9.0/PrintServiceGenerics.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("CalculoFigurasAbstractMethods")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("PrintServiceGenerics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc076741100b1969bf1e5d8cbd06d1ff120a0e6a")]
-[assembly: System.Reflection.AssemblyProductAttribute("CalculoFigurasAbstractMethods")]
-[assembly: System.Reflection.AssemblyTitleAttribute("CalculoFigurasAbstractMethods")]
+[assembly: System.Reflection.AssemblyProductAttribute("PrintServiceGenerics")]
+[assembly: System.Reflection.AssemblyTitleAttribute("PrintServiceGenerics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
