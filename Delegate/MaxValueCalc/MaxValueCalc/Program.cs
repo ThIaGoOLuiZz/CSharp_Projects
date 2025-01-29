ccce55d31d73ea7c41b24f544bbab48dffc94976
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-01-29 10:52:57 -0300

    Iniciado estudos de 'delegate'

---
+++ Delegate/MaxValueCalc/MaxValueCalc/Program.cs
@@ -0,0 +1,24 @@
+﻿using Course.Services;
+
+namespace MaxValueCalc
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+            BinaryNumericOperation op2 = CalculationService.Max;
+
+            double result = op(a, b);
+            Console.WriteLine(result);
+
+            result = op2(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
