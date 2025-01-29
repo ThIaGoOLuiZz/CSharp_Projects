ee2e9f3cded53a67b04ee2f3ba90588e0b7102d4
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-01-29 00:41:50 -0300

    Adicionado projeto de Dictionary

---
+++ ProgramasBasicos/ManipulaçãoDictionary/ManipulaçãoDictionary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace ManipulaçãoDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "Maria";
+            cookies["Email"] = "maria@gmail.com";
+            cookies["Fone"] = "999999999";
+            cookies["Fone"] = "000000000";
+
+            Console.WriteLine(cookies["Email"]);
+            Console.WriteLine(cookies["Fone"]);
+
+            cookies.Remove("Email");
+            
+            if (cookies.ContainsKey("Email")) Console.WriteLine(cookies["Email"]);
+            else Console.WriteLine("Não existe o email");
+
+            Console.WriteLine("Tamanho: " + cookies.Count);
+
+            Console.WriteLine("All Cookies");
+
+            foreach(var item in cookies)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+        }
+    }
+}
