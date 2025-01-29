55ed8811601f71453ad8af290aedcda6a3682747
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-01-28 23:16:49 -0300

    Adicionado projeto de manipulação de conjunto com HashSet

---
+++ OrientadoObjeto/ManupulandoConjuntoHashSet/ManupulandoConjuntoHashSet_SortedSet/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace ManupulandoConjuntoHashSet_SortedSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Computer"));
+
+            foreach (string i in set)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
