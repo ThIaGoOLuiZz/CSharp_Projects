cc2bae2d5b745ad7164785fdb89d4eedd65fbb29
ThIaGoOLuiZz <thiagoluizgon13@gmail.com>
2025-01-29 13:14:35 -0300

    Iniciado estudos do Linq

---
+++ Linq/FuncaoMultiplicarLinq/IniciandoLinq/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Linq;
+
+namespace FuncaoMultiplicarLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] {2,3,4,5}; //Especificação da origem dos dados
+
+            var result = numbers //Definindo a expressão de consulta
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
+
+            foreach (int x in result) //Executando a consulta
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
